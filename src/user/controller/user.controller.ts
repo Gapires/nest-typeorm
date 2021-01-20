a627c8e267da749453925e3a8bfe99dcb2922dfd
Gabriel Pires <gapires.gap@hotmail.com>
2021-01-19 21:59:24 -0300

    User entity services and controllers
    Creation of all methods of the services and controllers of the user entity, testing  the api using insomnia/postmen.

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UserService } from './../service/user.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { User } from '../models/user.interface';
 
-@Controller()
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  create(@Body() user: User): Observable<User> {
+    return this.userService.create(user);
+  }
+
+  @Get()
+  findAll(@Param() params): Observable<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Observable<User> {
+    return this.userService.findOne(Number(id));
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: string): Observable<any> {
+    return this.userService.deleteOne(Number(id));
+  }
+
+  @Put(':id')
+  updateOne(@Param('id') id: string, @Body() user: User): Observable<any> {
+    return this.userService.updateOne(Number(id), user);
+  }
+}
